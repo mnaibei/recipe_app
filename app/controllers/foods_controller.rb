bf4cf4e89f4d86ba3c0740969d12443fa49b12e1
mnaibei <muchajulius@gmail.com>
2023-07-31 12:40:10 +0300

    Modified controllers

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,33 +1,33 @@
-class FoodsController < ApplicationController
-  before_action :authenticate_user!
+# class FoodsController < ApplicationController
+#   before_action :authenticate_user!
 
-  def index
-    @foods = current_user.foods
-  end
+#   def indexs
+#     @foods = current_user.foods
+#   end
 
-  def new
-    @food = Food.new
-  end
+#   def new
+#     @food = Food.new
+#   end
 
-  def create
-    @food = current_user.foods.build(food_params)
+#   def create
+#     @food = current_user.foods.build(food_params)
 
-    if @food.save
-      redirect_to foods_url, notice: 'Food was successfully created.'
-    else
-      render :new
-    end
-  end
+#     if @food.save
+#       redirect_to foods_url, notice: 'Food was successfully created.'
+#     else
+#       render :new
+#     end
+#   end
 
-  def destroy
-    @food = current_user.foods.find(params[:id])
-    @food.destroy
-    redirect_to foods_url, notice: 'Food was successfully deleted.'
-  end
+#   def destroy
+#     @food = current_user.foods.find(params[:id])
+#     @food.destroy
+#     redirect_to foods_url, notice: 'Food was successfully deleted.'
+#   end
 
-  private
+#   private
 
-  def food_params
-    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
-  end
-end
+#   def food_params
+#     params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+#   end
+# end
