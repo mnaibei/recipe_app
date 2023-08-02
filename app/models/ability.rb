2e3b0362981db406b70fd7852595c14ea41700c4
mnaibei <muchajulius@gmail.com>
2023-08-02 12:08:37 +0300

    Fix cancancan issue

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,7 @@ class Ability
     else
       can :read, Recipe
       can :read, Food
+      can :read, RecipeFood
 
       can :create, Recipe
       can :update, Recipe, user_id: user.id
@@ -17,6 +18,10 @@ class Ability
       can :create, Food
       can :update, Food, user_id: user.id
       can :destroy, Food, user_id: user.id
+
+      can :create, RecipeFood
+      can :update, RecipeFood, food: { user_id: user.id }
+      can :destroy, RecipeFood, food: { user_id: user.id }
 
       can :destroy, Recipe, public: true, user_id: nil
       can :toggle_public, Recipe, user_id: user.id
